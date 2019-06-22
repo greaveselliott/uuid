0c7ed4a3be463ad80bacb68f610556a542daab26
Elliott Greaves <greaveselliott@me.com>
2019-06-22 18:24:11 +0100

    Added additional unit test, checking UUID is valid format.

--- src/uuid.spec.js
+++ src/uuid.spec.js
@@ -1,6 +1,13 @@
 import uuid from "./uuid";
 
 describe("uuid...", () => {
+  it("should generate a valid UUID.", () => {
+    const SUT = uuid();
+    const uuidRegex = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/;
+
+    expect(SUT).toMatch(uuidRegex);
+  });
+
   it("should generate 500,000 universally unique identifers.", () => {
     let hashTable = {};
     let testCases = 5e5;
